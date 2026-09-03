43dbe15edab1613a081e8e4399f54fb6cc9c75cc
69070045 <69070045@kmitl.ac.th>
2026-09-03 18:01:58 +0700

    Update code.py

--- oj3135/code.py
+++ oj3135/code.py
@@ -1,3 +1,22 @@
-"""c0de"""
-n, t, k = map(int, input().split())
-for i in range(n):
+"""code"""
+def main():
+    """code"""
+    n, k, t = map(int, input().split())
+
+    if t == 1:
+        print(1)
+        return
+    yunaii = 1
+    nub = 1
+
+    while True:
+        konenn = (yunaii - 1 + k) % n + 1
+        if konenn == 1:
+            break
+        if konenn == t:
+            nub += 1
+            break
+        nub += 1
+        yunaii = konenn
+    print(nub)
+main()
